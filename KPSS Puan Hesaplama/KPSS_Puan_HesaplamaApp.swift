50d44d403b8b8d3900696fbf58f98e4c484171d0
Fatih Durmaz <fatih.durmaz@outlook.com>
2024-01-30 23:19:25 +0300

    Initial Commit

---
+++ KPSS Puan Hesaplama/KPSS_Puan_HesaplamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KPSS_Puan_HesaplamaApp.swift
+//  KPSS Puan Hesaplama
+//
+//  Created by Fatih Durmaz on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct KPSS_Puan_HesaplamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
